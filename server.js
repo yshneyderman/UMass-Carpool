6eae9a4801c2abb0e71ac59068bd094114535c2f
aryan <aryansingh@umass.edu>
2019-05-18 07:01:46 +0530

    adding ejs, css

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
 
+app.get('/', function (req, res) {
+  res.render('index');
+})
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
